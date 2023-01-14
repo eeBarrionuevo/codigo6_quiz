2a19bcb8ad798a1632d61050e94c70a647c10c76
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-01-13 23:04:50 -0500

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> questions = [
+    "¿He dormido 8 horas?",
+    "¿La tierra es plana?",
+    "¿El hombre fue a la luna?",
+    "¿Ella me quiere?",
+    "¿He almozado hoy día?"
+  ];
+
+  List<Widget> scoreKeeper = [
+    Icon(
+      Icons.check,
+      color: Color(0xff3bceac),
+      size: 30.0,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff2b2d42),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 8,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Text(
+                  questions[2],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // SizedBox(
+          //   width: double.infinity,
+          //   child: ElevatedButton(
+          //     onPressed: () {},
+          //     child: Text(
+          //       "Hola",
+          //     ),
+          //   ),
+          // ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                onPressed: () {
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.close,
+                      color: Colors.white,
+                    ),
+                  );
+
+                  setState(() {});
+                },
+                color: Color(0xff3bceac),
+                child: Text(
+                  "Verdadero",
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                onPressed: () {},
+                color: Color(0xffff6b6b),
+                child: Text(
+                  "Falso",
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: scoreKeeper,
+          ),
+        ],
+      ),
+    );
+  }
+}
