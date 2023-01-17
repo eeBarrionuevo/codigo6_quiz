3153c8d6b197385632f725c893e4a1d5b2e55f31
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-01-16 23:07:42 -0500

    Mods

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,24 +8,40 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   QuizBrain mandarina = QuizBrain();
-
-  // List<String> questions = [
-  //   "¿He dormido 8 horas?", //false
-  //   "¿La tierra es plana?", //false
-  //   "¿El hombre fue a la luna?", //true
-  //   "¿Ella me quiere?", //false
-  //   "¿He almozado hoy día?" //true
-  // ];
-
-  // List<bool> answers = [
-  //   false,
-  //   false,
-  //   true,
-  //   false,
-  //   true,
-  // ];
-
   List<Icon> scoreKeeper = [];
+
+  void checkQuestion(bool type) {
+    //Obtenemos la respuesta correcta de la pregunta
+    // bool correctAnswer =
+    //     mandarina.questions[questionNumber].questionAnswer;
+
+    bool correctAnswer = mandarina.getQuestionAnswer();
+
+    //Comparamos la respuesta correcta con el valor verdaddero
+    if (correctAnswer == type) {
+      //Si la respuesta correcta es verdadera
+      //Se agrega un icono a la lista de respuestas
+      scoreKeeper.add(
+        Icon(
+          Icons.check,
+          color: Colors.green,
+        ),
+      );
+    } else {
+      //Si la respuesta correcta es falsa
+      //Se agrega un icono de error a la lista de respuestas
+      scoreKeeper.add(
+        Icon(
+          Icons.close,
+          color: Colors.red,
+        ),
+      );
+    }
+    //Se redibuja el widget - Se ejecuta el método build
+    setState(() {});
+    //El valor del contador aumenta en uno
+    mandarina.nextQuestion();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,36 +82,7 @@ class _HomePageState extends State<HomePage> {
               padding: const EdgeInsets.all(8.0),
               child: MaterialButton(
                 onPressed: () {
-                  //Obtenemos la respuesta correcta de la pregunta
-                  // bool correctAnswer =
-                  //     mandarina.questions[questionNumber].questionAnswer;
-
-                  bool correctAnswer = mandarina.getQuestionAnswer();
-
-                  //Comparamos la respuesta correcta con el valor verdaddero
-                  if (correctAnswer == true) {
-                    //Si la respuesta correcta es verdadera
-                    //Se agrega un icono a la lista de respuestas
-                    scoreKeeper.add(
-                      Icon(
-                        Icons.check,
-                        color: Colors.green,
-                      ),
-                    );
-                  } else {
-                    //Si la respuesta correcta es falsa
-                    //Se agrega un icono de error a la lista de respuestas
-                    scoreKeeper.add(
-                      Icon(
-                        Icons.close,
-                        color: Colors.red,
-                      ),
-                    );
-                  }
-                  //Se redibuja el widget - Se ejecuta el método build
-                  setState(() {});
-                  //El valor del contador aumenta en uno
-                  mandarina.nextQuestion();
+                  checkQuestion(true);
                 },
                 color: Color(0xff3bceac),
                 child: Text(
@@ -109,34 +96,7 @@ class _HomePageState extends State<HomePage> {
               padding: const EdgeInsets.all(8.0),
               child: MaterialButton(
                 onPressed: () {
-                  //Obtenemos la respuesta correcta de la pregunta
-
-                  bool correctAnswer = mandarina.getQuestionAnswer();
-
-                  //Comparamos la respuesta correcta con el valor verdaddero
-                  if (correctAnswer == false) {
-                    //Si la respuesta correcta es verdadera
-                    //Se agrega un icono a la lista de respuestas
-                    scoreKeeper.add(
-                      Icon(
-                        Icons.check,
-                        color: Colors.green,
-                      ),
-                    );
-                  } else {
-                    //Si la respuesta correcta es falsa
-                    //Se agrega un icono de error a la lista de respuestas
-                    scoreKeeper.add(
-                      Icon(
-                        Icons.close,
-                        color: Colors.red,
-                      ),
-                    );
-                  }
-                  //Se redibuja el widget - Se ejecuta el método build
-                  setState(() {});
-                  //El valor del contador aumenta en uno
-                  mandarina.nextQuestion();
+                  checkQuestion(false);
                 },
                 color: Color(0xffff6b6b),
                 child: Text(
